1b1818059e1c526fba7bd53e890122ca66128090
Ben <ben.j.marum@gmail.com>
2023-05-01 21:19:17 -0600

    Complete Lab 7p2f

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // code your solution here
+function saturdayFun(activity){
+if (Boolean(activity)===false) {
+    const activity = 'roller-skate'
+    console.log(`This Saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+} else {
+    console.log(`This Saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+}
+};
+
+
+
+function mondayWork(activity){
+    if (Boolean(activity)===false) {
+        const activity = 'go to the office'
+        console.log(`This Monday, I will ${activity}.`)
+        return `This Monday, I will ${activity}.`
+    } else {
+        console.log(`This Monday, I will ${activity}.`)
+        return `This Monday, I will ${activity}.`
+    }
+};
+
+function wrapAdjective(visFlair='*'){
+    const vF = visFlair;
+    return function (param = 'special'){
+            console.log(`You are ${vF}${param}${vF}!`)
+    };
+};
+
+// function demoChain(name) {
+//     const part1 = "hi";
+//     return function () {
+//       const part2 = "there";
+//       return function () {
+//         console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+//       };
+//     };
+//   }
+
+wrapAdjective('||')('a dedicated programmer');
+
+wrapAdjective()();
+
+// saturdayFun('bobsled');
+// saturdayFun('kayak');
+// saturdayFun('hunt bigfoot');
+// saturdayFun();
+
+// mondayWork('dream');
+// mondayWork('make salsa');
+// mondayWork();
